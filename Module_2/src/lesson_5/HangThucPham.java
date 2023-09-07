e18cb468e2717de102a543fef5f5b68be8a67b4e
HueVuong12 <ngochuevuong12@gmail.com>
2023-09-07 23:46:22 +0700

    lesson_5

--- Module_2/src/lesson_5/HangThucPham.java
+++ Module_2/src/lesson_5/HangThucPham.java
@@ -1,20 +1,97 @@
 package lesson_5;
 
 import java.sql.Date;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class HangThucPham {
 	private String maHang;
 	private String tenHang;
 	private float donGia;
-	private Date ngaySanXuat;
-	private Date ngayHetHan;
-	public HangThucPham(String maHang, String tenHang, float donGia, Date ngaySanXuat, Date ngayHetHan) {
+	private LocalDate ngaySanXuat;
+	private LocalDate ngayHetHan;
+	private DateTimeFormatter formatter;
+
+	public HangThucPham(String maHang, String tenHang, float donGia, LocalDate ngaySanXuat, LocalDate ngayHetHan) {
+		super();
 		this.maHang = maHang;
 		this.tenHang = tenHang;
 		this.donGia = donGia;
 		this.ngaySanXuat = ngaySanXuat;
 		this.ngayHetHan = ngayHetHan;
 	}
-	
+
+	public HangThucPham(String maHang) {
+		if (maHang.isEmpty()) {
+			throw new IllegalArgumentException("Mã hàng không được rỗng");
+		}
+		this.maHang = maHang;
+		this.tenHang = "xxx";
+		this.donGia = 0;
+		this.ngaySanXuat = LocalDate.now();
+		this.ngayHetHan = this.ngaySanXuat;
+	}
+
+	public String getMaHang() {
+		return maHang;
+	}
+
+	public void setMaHang(String maHang) {
+		this.maHang = maHang;
+	}
+
+	public String getTenHang() {
+		return tenHang;
+	}
+
+	public void setTenHang(String tenHang) {
+		this.tenHang = tenHang;
+	}
+
+	public float getDonGia() {
+		return donGia;
+	}
+
+	public void setDonGia(float donGia) {
+		this.donGia = donGia;
+	}
+
+	public LocalDate getNgaySanXuat() {
+		return ngaySanXuat;
+	}
+
+	public void setNgaySanXuat(LocalDate ngaySanXuat) {
+		this.ngaySanXuat = ngaySanXuat;
+	}
+
+	public LocalDate getNgayHetHan() {
+		return ngayHetHan;
+	}
+
+	public void setNgayHetHan(LocalDate ngayHetHan) {
+		this.ngayHetHan = ngayHetHan;
+	}
+
+	@Override
+	public String toString() {
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-YYYY");
+		DecimalFormat df = new DecimalFormat("#,##0.00");
+		return String.format("  " + maHang + "   " + tenHang + "                 " + df.format(donGia) + "      "
+				+ dtf.format(ngaySanXuat) + "            " + dtf.format(ngayHetHan) + "            "
+				+ kiemTraHetHanSuDung());
+	}
+	/**
+	 * nếu đúng trả true, ngược lại false
+	 * 
+	 * @return
+	 */
+	public boolean kiemTraHetHanSuDung() {
+		if (LocalDate.now().isAfter(this.ngayHetHan))
+			return true;
+		return false;
+	}
+
 }
--- Module_2/src/lesson_5/Test.java
+++ Module_2/src/lesson_5/Test.java
@@ -1,10 +1,24 @@
 package lesson_5;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Test {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		HangThucPham hang1 = new HangThucPham("001", "gao", 20, 20);
+//LocalDate.now() hiện này hiện tại
+		HangThucPham htp = new HangThucPham("001", "Gao", 100000, LocalDate.of(2018, 7, 10), LocalDate.of(2018, 7, 10));
+		HangThucPham htp1 = new HangThucPham("002", "Mi", 500000, LocalDate.of(2018, 8, 1), LocalDate.of(2018, 9, 1));
+		HangThucPham htp2 = new HangThucPham("003", "Nuoc", 500000, LocalDate.of(2019, 2, 10),LocalDate.of(2024, 2, 12));
+		System.out.println(String.format("%-5s %-20s %1s %20s %20s %15s", "ma_hang", "ten_hang", "don_gia", "ngay_san_xuat",
+				"ngay_het_han", "Ghi_chu"));
+		System.out.println(htp);
+		System.out.println(htp1);
+		System.out.println(htp2);
+//		System.out.println(df.format(htp.getDonGia()));
+//		System.out.println(dtf.format(htp.getNgayHetHan()));
 	}
 }
